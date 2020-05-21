f5525ec26ab9e0163e2ef0698c1510e86cd1e0a4
Lucas Pugliesi Ferreira <ferreiraplucas@gmail.com>
2020-05-21 17:31:59 +0100

    Flexbox to grid

--- script.js
+++ script.js
@@ -3,18 +3,9 @@ const divEnglish = document.getElementById('en')
 const divPortuguese = document.getElementById('pt')
 
 function toggleLanguage() {
-  if (language.text == 'EN') {
-    language.innerText = 'PT'
-    divEnglish.style.display = 'inline'
-    divPortuguese.style.display = 'none'
-  } else {
-    language.innerText = 'EN'
-    divEnglish.style.display = 'none'
-    divPortuguese.style.display = 'inline'
-  }
+  (language.text == 'EN') ? language.innerText = 'PT' : language.innerText = 'EN'
+  divEnglish.classList.toggle('hidden')
+  divPortuguese.classList.toggle('hidden')
 }
 
-language.onclick = function() {
-  toggleLanguage()
-}
-
+language.addEventListener('click', toggleLanguage)
